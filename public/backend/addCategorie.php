914f305f8f01019784b6a961980ed975b24360b2
Miltiadis Orfanidis <mio@tradebyte.biz>
2017-11-20 14:35:40 +0100

    Directories

---
+++ public/backend/addCategorie.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mio
+ * Date: 20.11.17
+ * Time: 11:10
+ */
---
+++ public/frontend/view/login/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="../../misc/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="login.css">
+	<meta charset="UTF-8">
+	<title>Login</title>
+</head>
+<body>
+
+	<div class="menu">
+		<?php include '../menuBar/menuBar.php'; ?>
+	</div>
+	<div class="container-fluid">
+		<form>
+			<div class="row">
+				<div class="input-group">
+					<span class="input-group-addon" id="basic-addon1">@</span>
+					<input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+				</div>
+			</div>
+		</form>
+	</div>
+	<script src="../../misc/js/bootstrap.min.js"></script>
+</body>
+</html>
