b94193aafec799405cbd12610c27c233d61b53ff
Miltiadis Orfanidis <mio@tradebyte.biz>
2017-11-20 15:43:08 +0100

    login view

--- public/frontend/view/edit/edit.php
+++ public/frontend/view/edit/edit.php
@@ -2,9 +2,11 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Title</title>
+	<title>Edit</title>
 </head>
 <body>
-
+	<div class="menu">
+		<?php include '../menuBar/menuBar.php'; ?>
+	</div>
 </body>
 </html>
--- public/frontend/view/login/login.php
+++ public/frontend/view/login/login.php
@@ -1,26 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<link rel="stylesheet" type="text/css" href="../../misc/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="login.css">
-	<meta charset="UTF-8">
 	<title>Login</title>
 </head>
 <body>
-
 	<div class="menu">
 		<?php include '../menuBar/menuBar.php'; ?>
 	</div>
 	<div class="container-fluid">
-		<form>
+		<div class="form-group">
 			<div class="row">
 				<div class="input-group">
-					<span class="input-group-addon" id="basic-addon1">@</span>
+					<span class="input-group-addon" id="basic-addon1"><a href="#"><span class="glyphicon glyphicon-user"></span></a></span>
 					<input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
 				</div>
 			</div>
-		</form>
+		</div>
+		<div class="form-group">
+			<div class="row">
+				<div class="input-group">
+					<span class="input-group-addon" id="basic-addon1"><a href="#"><span class="glyphicon glyphicon-pencil"></span></a></span>
+					<input type="text" class="form-control" placeholder="Password" aria-describedby="basic-addon1">
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<button class="btn btn-success" id="login">Login</button>
+			<button class="btn btn-danger" id="decline">Decline</button>
+		</div>
 	</div>
-	<script src="../../misc/js/bootstrap.min.js"></script>
 </body>
 </html>
